09a748bfa20ac69660c5f75068ffe26985ba2be7
Rajwinder Mahal <rajwinder@mahal.dev>
2023-01-31 23:57:08 -0500

    Prepare to deploy

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,11 +1,14 @@
+import meta from './page.metadata.json'
+
 export default function Head() {
   return (
     <>
-      <title>Rajwinder Mahal | RSM</title>
+      <title>{meta.title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="referrer" content="strict-origin-when-cross-origin" />
-      <meta name="description" content="Rajwinder is a graduate student in the Computer Science program at Columbia University where he specializes in Machine Learning. He holds a Master's degree in Innovation and Entrepreneurship from the University of California, Irvine. His research interests include Machine Learning, NLP, and Cloud Computing." />
+      <meta name="description" content={meta.description} />
+      <link rel="canonical" href="https://www.mahal.dev" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
