f9ed9b9823b8e2e1962a58577928f690118d8a8b
Rajwinder Mahal <rajwinder@mahal.dev>
2023-02-01 01:01:46 -0500

    Reset nextjs distDir

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   reactStrictMode: true,
   poweredByHeader: false,
-  distDir: 'build',
 }
 
 module.exports = nextConfig
