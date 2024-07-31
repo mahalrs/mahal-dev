ec7aee251db7226ae748555119f23e899b4ccd69
Rajwinder Mahal <rajwinder@mahal.dev>
2024-07-31 01:59:53 -0400

    Add error pages

---
+++ src/app/error.tsx
@@ -0,0 +1,48 @@
+// Copyright 2024 The RSM Authors
+
+'use client'
+
+import { useEffect } from 'react'
+import NextLink from 'next/link'
+import { Button } from '@nextui-org/button'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // TODO: Log the error to an error reporting service
+    console.error('error', error)
+  }, [error])
+
+  return (
+    <div className='flex min-h-[75svh] flex-col justify-center px-6 py-12 sm:px-12 md:py-16'>
+      <div className='mx-auto'>
+        <h2 className='text-3xl font-bold'>Something went wrong!</h2>
+        <p className='mt-6 text-default-700'>
+          {`We're having trouble processing your request.`}
+        </p>
+        <div className='mt-12 flex flex-wrap gap-4'>
+          <Button
+            onClick={() => reset()}
+            size='sm'
+            variant='bordered'
+          >
+            Try again
+          </Button>
+          <Button
+            as={NextLink}
+            href='/'
+            size='sm'
+            className='bg-foreground text-background'
+          >
+            Return home
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+// Copyright 2024 The RSM Authors
+
+import { Metadata } from 'next'
+import NextLink from 'next/link'
+import { Button } from '@nextui-org/button'
+
+export const metadata: Metadata = {
+  title: '404 Not Found',
+}
+
+export default function NotFound() {
+  return (
+    <main className='flex min-h-[75svh] flex-col justify-center px-6 py-12 sm:px-12 md:py-16'>
+      <div className='mx-auto'>
+        <h2 className='text-3xl font-bold'>Not Found</h2>
+        <p className='mt-6 text-default-700'>
+          {`Sorry we couldn't find what you were looking for.`}
+        </p>
+        <Button
+          as={NextLink}
+          href='/'
+          size='sm'
+          className='mt-12 bg-foreground text-background'
+        >
+          Return home
+        </Button>
+      </div>
+    </main>
+  )
+}
