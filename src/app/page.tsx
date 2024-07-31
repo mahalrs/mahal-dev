d8acaab0fb3ec0a3cfadc6f9ea5dc2d3523150fc
Rajwinder Mahal <rajwinder@mahal.dev>
2024-07-31 04:09:30 -0400

    Update bio on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@
 
 import type { Metadata } from 'next'
 
+import { Button } from '@nextui-org/button'
+import { Link } from '@nextui-org/link'
+
+import { GitHubIcon } from '@/components/icons/github'
+import { LinkedInIcon } from '@/components/icons/linkedin'
+
 export const metadata: Metadata = {
   title: 'Rajwinder Mahal · RSM',
   description:
@@ -28,7 +34,46 @@ const jsonld = {
 export default function Home() {
   return (
     <>
-      <main>RSM</main>
+      <main className='flex min-h-[75svh] flex-col justify-center px-6 py-12 sm:px-12 md:py-16'>
+        <div className='mx-auto'>
+          <h1 className='text-3xl font-bold sm:text-4xl md:text-6xl md:leading-tight lg:text-8xl'>
+            Rajwinder Mahal
+          </h1>
+          <div className='max-w-2xl text-sm text-default-700 sm:text-base md:text-lg'>
+            <p className='mt-8 sm:mt-12 md:mt-16'>
+              Rajwinder Mahal is a skilled machine learning engineer, passionate
+              about open-source software.
+            </p>
+            <p className='mt-6 md:mt-8'>
+              Rajwinder has experience in building scalable machine learning
+              systems and has worked on projects in machine learning, natural
+              language processing, computer vision, and recommender systems.
+            </p>
+          </div>
+          <div className='mt-12 flex flex-wrap gap-4'>
+            <Button
+              as={Link}
+              href='https://www.linkedin.com/in/mahalrajwinder'
+              isExternal
+              showAnchorIcon
+              variant='bordered'
+              startContent={<LinkedInIcon size={16} />}
+            >
+              LinkedIn
+            </Button>
+            <Button
+              as={Link}
+              href='https://github.com/mahalrs'
+              isExternal
+              showAnchorIcon
+              variant='bordered'
+              startContent={<GitHubIcon size={16} />}
+            >
+              GitHub
+            </Button>
+          </div>
+        </div>
+      </main>
       <script
         type='application/ld+json'
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonld) }}
